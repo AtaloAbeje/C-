0e63f361b01c31ff69998a66f4c37f5d753360d3
Atalo Abeje <44359191+AtaloAbeje@users.noreply.github.com>
2018-11-29 10:12:22 +0200

    Csharp / Home_work

---
+++ Home_work/03_Question1 - Clock/03_Question1 - Clock/Clock.cs
@@ -0,0 +1,83 @@
+﻿using System;
+
+namespace _03_Question1___Clock
+{
+    class Clock
+    {
+        private int hours;
+
+        public int Hours
+        {
+            get { return hours; }
+            set { hours = value; }
+        }
+        //
+        private int minutes;
+
+        public int Minutes
+        {
+            get { return minutes; }
+            set { minutes = value; }
+        }
+        //
+        private int seconds;
+
+        public int Seconds
+        {
+            get { return seconds; }
+            set { seconds = value; }
+        }
+        //
+        private int milliseconds;
+
+        public int Milliseconds
+        {
+            get { return milliseconds; }
+            set { milliseconds = value; }
+        }
+
+        public void tick()
+        {
+            milliseconds = milliseconds + 100;
+            seconds = seconds + milliseconds / 1;
+            minutes = minutes + seconds / 60;
+            hours = hours + minutes / 60;
+
+            milliseconds = milliseconds % 100;
+            seconds = seconds % 60;
+            minutes = minutes % 60;
+            hours = hours % 24;
+        }
+
+        public string show()
+        {
+            return $"The current time is: {Hours}:{Minutes}:{Seconds}:{Milliseconds}";
+        }
+
+        public void setMidNight()
+        {
+            Milliseconds= 0;
+            Seconds = 0;
+            Minutes = 0;
+            Hours = 24;
+        }
+
+        public void setMidDay()
+        {
+            Milliseconds = 0;
+            Seconds = 0; 
+            Minutes = 0;
+            Hours = 12;
+        }
+
+        public Clock(int hours, int minutes, int seconds, int miliseconds) // פונקציה לקבלת פרמטרים
+        { 
+            Hours = hours;
+            Minutes = minutes;
+            Seconds = seconds;
+            Milliseconds = miliseconds;
+        }
+        public Clock() { }
+
+    }
+}
---
+++ Home_work/03_Question1 - Clock/03_Question1 - Clock/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+namespace _03_Question1___Clock
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // answar 1.5.1 + get info from user
+            Clock MyClock = new Clock();
+            do
+            {                                        // Hours between 0 and 12;
+                Console.WriteLine("Please Current Enter Hour:");
+                MyClock.Hours = Convert.ToInt32(Console.ReadLine());
+            } while (MyClock.Hours < 0 || MyClock.Hours > 13);
+
+            do
+            {                                        // Minutes between 0 and 60;
+                Console.WriteLine("Please Current Enter Minutes:");
+            MyClock.Minutes = Convert.ToInt32(Console.ReadLine());
+            } while (MyClock.Minutes < 0 || MyClock.Minutes > 60);
+
+            do
+            {                                        // Seconds between 0 and 12;
+                Console.WriteLine("Please Current Enter Seconds:");
+                MyClock.Seconds = Convert.ToInt32(Console.ReadLine());
+            } while (MyClock.Seconds < 0 || MyClock.Seconds > 60);
+
+            do
+            {                                        // Milli Secondes between 0 and 1000;
+                Console.WriteLine("Please Current Enter Milliseconds:");
+                MyClock.Milliseconds = Convert.ToInt32(Console.ReadLine());
+            } while (MyClock.Milliseconds < 0 || MyClock.Milliseconds > 1000);
+
+            Console.Clear(); // answar 1.5.2
+
+            Console.WriteLine(MyClock.show()); // print My Clock
+
+            Console.WriteLine(); // answar 1.5.3 + 4 
+            MyClock.Milliseconds += 2;
+            Console.WriteLine($"{MyClock.show()} ->> My Clock after + 2 Milliseconds");
+
+            Console.WriteLine(); // answar 1.5.5
+            MyClock.setMidDay();
+            Console.WriteLine($"{MyClock.show()} ->> My Clock at Mid Day");
+
+            Console.WriteLine(); // answar 1.5.6 + 7
+            MyClock.Milliseconds += 3;
+            Console.WriteLine(MyClock.show());
+
+            Clock YourClock = new Clock(); // answar 1.5.8
+
+            Console.WriteLine(); // answar 1.5.9 + 10
+            YourClock.setMidNight();
+            Console.WriteLine($"{YourClock.show()} ->> Mid Night");
+
+            /// Task.03.Question.1 -->> Atalo
+        }
+    }
+}
